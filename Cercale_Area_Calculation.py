cfbcaed2d18be3e1687ca9c0480d64e35125ad0f
mchelbabi <122228494+mchelbabi@users.noreply.github.com>
2023-01-11 20:54:07 +0300

    Add files via upload
    This is Cercle area calculation python code

---
+++ Cercale_Area_Calculation.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  6 10:37:25 2023
+
+@author: chelbabi
+"""
+
+#this is mu first python program 
+#Variable declarataion 
+
+# Radius = float(input('Provide cercale radius please :'))
+# Pi=3.14
+# Cercale_Surface = Pi * Radius**2
+# print ('you entred radius :',Radius,'and area of cercale is',Cercale_Surface)
+################################################################################
+# Convert Fahrent to Celsius
+# Fahrenheit_Tmp = float(input('please enter temprature to covert:')) 
+# Celsius_Tmp = (Fahrenheit_Tmp-32)*(5/9)
+# print('The Celsius Temprture is :',Celsius_Tmp)
+
+# Sum of to Numbers Code
+Num1= float(input('enter first number to sum :'))
+Num2= float(input('enter second number to sum :'))
+Sum = Num1+Num2
+print('entered first number is :',Num1,' ','entered second number is :',Num2,'\n', 'Sum is :',Sum )
